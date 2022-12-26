901d1d546b5ffa5756715f9af72c84840eede380
Joshua Wu <Joshua.wu.0910@gmail.com>
2022-12-26 00:36:07 -0800

    chore: cleanup, add new files, install deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Stack } from "@mui/material";
+import "./App.scss";
+import TextAreaViewer from "./components/TextAreaViewer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Stack sx={{ m: "auto", d: "flex" }} direction="row" spacing={3}>
+        <TextAreaViewer id="input"></TextAreaViewer>
+        <TextAreaViewer id="output"></TextAreaViewer>
+      </Stack>
+    </Box>
   );
 }
 
---
+++ src/utils/stringHelpers.ts
@@ -0,0 +1,3 @@
+export const addTemplateStrings = (argStrings: string): string => {
+  return `{{ ${argStrings} }}`;
+};
