3ae0e7cda7d262b0db2add8da21b28bf2bfdf9da
Joshua Wu <Joshua.wu.0910@gmail.com>
2022-12-30 15:50:06 -0800

    feat: added helper functions to make this mvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,38 @@
-import './App.scss';
+import "./App.scss";
 
-import { Box, Stack } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { Box, Stack } from "@mui/material";
+import { useEffect, useState } from "react";
+import { parseKeys } from "./utils/helpers";
 
-export interface ICommand {
-  Array: "cslice";
-  Object: "sdict";
+export enum ICommand {
+  Array = "cslice",
+  Object = "sdict",
+}
+
+export interface IAuthor {
+  name: string;
+  url: string;
+  icon_url: string;
+}
+export interface IField {
+  name: string;
+  value: string;
+  inline?: boolean;
+}
+
+export interface IFooter {
+  text: string;
+  icon_url: string;
 }
 
 export interface IEmbed {
-  author?: {
-    name: string;
-    url: string;
-    icon_url: string;
-  };
+  author?: IAuthor;
   channel?: string;
   color?: number;
   content?: string;
   description?: string;
-  fields: {
-    name: string;
-    value: string;
-    inline?: boolean;
-  }[];
-  footer?: { text: string; icon_url: string };
+  fields: IField[];
+  footer?: IFooter;
   image?: { url: string };
   message?: string;
   thumbnail?: { url: string };
@@ -32,16 +41,47 @@ export interface IEmbed {
   url?: string;
 }
 
-function App() {
-  const [inputValue, setInputValue] = useState("");
+const sampleJSON = JSON.stringify({
+  title: "Discord bot",
+  description:
+    "Discohook has a bot as well, it's not strictly required to send messages it may be helpful to have it ready.\n\nBelow is a small but incomplete overview of what the bot can do for you.",
+  color: 5814783,
+  fields: [
+    {
+      name: "Getting special formatting for mentions, channels, and emoji",
+      value:
+        "The **/format** command of the bot can give you special formatting for use in Discord messages that lets you create mentions, tag channels, or use emoji ready to paste into the editor!\n\nThere are [manual ways](https://discord.dev/reference#message-formatting) of doing this, but it's very error prone. The bot will make sure you'll always get the right formatting for your needs.",
+    },
+    {
+      name: "Creating reaction roles",
+      value:
+        "You can manage reaction roles with the bot using the **/reaction-role** command.\n\nThe set-up process is very intuitive: type out **/reaction-role create**, paste a message link, select an emoji, and pick a role. Hit enter and you're done, your members can now react to any of your messages to pick their roles.",
+    },
+    {
+      name: "Recover Discohook messages from your server",
+      value:
+        "It can also restore any message sent in your Discord server for you via the apps menu.\n\nTo get started, right-click or long-press on any message in your server, press on apps, and then press **Restore to Discohook**. It'll send you a link that leads to the editor page containing the message you selected!",
+    },
+    {
+      name: "Other features",
+      value:
+        "Discohook can also grab images from profile pictures or emoji, manage your webhooks, and more. Invite the bot and use **/help** to learn about all the bot offers!",
+    },
+  ],
+});
 
-  const handleJSONInput = (json: string): string => {
-    return "";
-  };
+function App() {
+  const [inputValue, setInputValue] = useState(sampleJSON);
+  const [outputValue, setOutputValue] = useState("");
 
   useEffect(() => {
     console.log("inputValue :>> ", inputValue);
-
+    try {
+      const output = parseKeys(JSON.parse(inputValue));
+      setOutputValue(output);
+    } catch (error) {
+      console.log("inputValue", JSON.stringify(inputValue, null, 2));
+    }
     /*
       Send current string into json validator?
       Use json validation lib?
@@ -81,6 +121,7 @@ function App() {
           cols={50}
           rows={30}
           id="output"
+          value={outputValue}
         ></textarea>
       </Stack>
     </Box>
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,14 +1,67 @@
+import { ICommand } from "../App";
 
-import { ICommand, IEmbed } from "../App";
-
-export const addCommandWrapper = (argStrings: string): string => {
-  return `{{ ${argStrings} }}`;
+export const addCommandWrapper = (
+  key: string,
+  argStrings: string | string[]
+): string => {
+  return `{{ ${key} ${argStrings}\n}}`;
 };
 
-export const makeCommandFromArgs = (type: ICommand, args: string[]): string => {
-  return ``;
-}
+export const addFunctionWrapper = (
+  functionName: string,
+  args: string[]
+): string => {
+  return `( ${functionName} ${args.join(" ")}\n\t)`;
+};
 
-export const parseKeys = (embed: IEmbed): string[] => {
-  return [];
-}
+export const makeCommandFromArgs = (
+  key: string,
+  args: string[] | { [key: string]: string },
+  type?: ICommand
+): string => {
+  let returnArg = "";
+  if (Array.isArray(args)) {
+    returnArg = `"${key}" ${addFunctionWrapper(
+      "cslice",
+      args.map((arg) => arg)
+    )}`;
+  } else if (typeof args === "object") {
+    returnArg = `\t"${key}" ${addFunctionWrapper(
+      "sdict",
+      Object.entries(args).map(
+        ([key, value]) => `\n\t\t"${key}" ${JSON.stringify(value)}`
+      )
+    )}`;
+  } else if (typeof args === "string" || typeof args === "number") {
+    returnArg = `"${key}" ${JSON.stringify(args)}`;
+  } else {
+  }
+  return `\n${returnArg}`;
+};
+
+export const parseKeys = (embed: Record<string, any>): string => {
+  const embedCommandArray = Object.keys(embed)
+    .map((key: string) => {
+      switch (key) {
+        case "author":
+        case "footer":
+        case "image":
+        case "thumbnail":
+          return makeCommandFromArgs(key, embed[key], ICommand.Object);
+        case "fields":
+          const fields = embed.fields.map((field: Record<string, string>) => {
+            return makeCommandFromArgs(key, field, ICommand.Object);
+          });
+
+          return makeCommandFromArgs(key, fields, ICommand.Array);
+        default:
+          return makeCommandFromArgs(key, embed[key]);
+      }
+    })
+    .join("");
+
+  return addCommandWrapper(
+    "$embed := cembed",
+    `( sdict\n${embedCommandArray} )`
+  );
+};
