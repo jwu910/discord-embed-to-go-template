2b3afb3cc16a1d1052b173b74bbc64460185380b
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-01-24 21:46:35 -0800

    chore: add styles so not white

--- src/App.tsx
+++ src/App.tsx
@@ -57,9 +57,10 @@ function App() {
 
   return (
     <Box sx={{ padding: 2, margin: "auto", width: "70%" }}>
+      <div className="header-details"></div>
       <h2>Embed to Go Templates</h2>
       <p>Convert JSON to go template structs for Yagpdb</p>
-      <Stack sx={{ margin: "auto" }} direction="row" spacing={3}>
+      <Stack sx={{ margin: "auto" }} className="text-areas" direction="row" spacing={3}>
         <textarea
           style={{ flexGrow: 1 }}
           cols={50}
