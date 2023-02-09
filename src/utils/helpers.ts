b4250626fcf5bca78eb5b6b612d07bc487fe5ffc
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-02-09 14:17:47 -0800

    chore: fix helpers syntax, update pacakge lock

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,93 +2,101 @@ import { IField } from "../App";
 
 // Function for wrapping the entire embed after parsing
 export const addCommandWrapper = (
-    key: string,
-    argStrings: string | string[]
-  ): string => {
-    return `{{ ${key} ${argStrings}\n}}`;
-  };
+  key: string,
+  argStrings: string | string[]
+): string => {
+  return `{{ ${key} ${argStrings}\n}}`;
+};
 
 // Function for handling single string fields
 export const handleStringEmbed = (
-    field: string,
-    fieldValue: string
-): string => { 
-    return `"${field}" "${fieldValue}"\n`
+  field: string,
+  fieldValue: string
+): string => {
+  return `"${field}" "${fieldValue}"\n`;
 };
 
 // Function for handling color string field
 export const handleColorEmbed = (
-    field: string,
-    fieldValue: number | null
+  field: string,
+  fieldValue: number | null
 ): string => {
-    if (fieldValue === null) {
-        return `"${field}" nil\n`;
-    } else {
-        return `"${field}" ${fieldValue}\n`;
-    }
+  if (fieldValue === null) {
+    return `"${field}" nil\n`;
+  } else {
+    return `"${field}" ${fieldValue}\n`;
+  }
 };
 
 // Function for handling fields with object values
 export const handleObjectEmbed = (
-    field: string,
-    fieldValue: object,
+  field: string,
+  fieldValue: object
 ): string => {
-    if (field === "fields") {
-        let objectEmbedStr = '( sdict\n'
-        Object.entries(fieldValue).map(([key, value]) => {
-            objectEmbedStr = objectEmbedStr + `"${key}" "${value}"\n`
-        });
-        objectEmbedStr = objectEmbedStr + ' )\n'
-        return `${objectEmbedStr}`
-    } else {
-        let objectEmbedStr = '( sdict\n';
-        let dictString = '';
-        Object.entries(fieldValue).map(([key, value]) => { 
-            dictString = dictString + `"${key}" "${value}"\n`
-        });
-        objectEmbedStr = objectEmbedStr + dictString + ")\n";
-        return `"${field}" ${objectEmbedStr}`
-    }
-}
-
-// Function for handling the 'fields' field 
-export const handleArrayEmbed = (
-    field: string,
-    fieldValue: IField[]
-): string => {
-    let sliceEmbedStr = '( cslice\n'
-    fieldValue.forEach(i => {
-        let objectEmbed = handleObjectEmbed(field, i)
-        sliceEmbedStr = sliceEmbedStr + objectEmbed;
+  if (field === "fields") {
+    let objectEmbedStr = "( sdict\n";
+    Object.entries(fieldValue).map(([key, value]) => {
+      objectEmbedStr = objectEmbedStr + `"${key}" "${value}"\n`;
     });
-    return `"${field}" ${sliceEmbedStr}`;
+    objectEmbedStr = objectEmbedStr + " )\n";
+    return `${objectEmbedStr}`;
+  } else {
+    let objectEmbedStr = "( sdict\n";
+    let dictString = "";
+    Object.entries(fieldValue).map(([key, value]) => {
+      dictString = dictString + `"${key}" "${value}"\n`;
+    });
+    objectEmbedStr = objectEmbedStr + dictString + ")\n";
+    return `"${field}" ${objectEmbedStr}`;
+  }
+};
+
+// Function for handling the 'fields' field
+export const handleArrayEmbed = (
+  field: string,
+  fieldValue: IField[]
+): string => {
+  let sliceEmbedStr = "( cslice\n";
+  fieldValue.forEach((i) => {
+    let objectEmbed = handleObjectEmbed(field, i);
+    sliceEmbedStr = sliceEmbedStr + objectEmbed;
+  });
+  return `"${field}" ${sliceEmbedStr}`;
 };
 
 export const parseKeys = (embed: Record<string, any>): string => {
-    const embedCommandArray = Object.keys(embed)
-        .map((key: string) => {
-            switch (key) {
-                case "color":
-                    return handleColorEmbed(key, embed[key]);
-                case "fields":
-                    return handleArrayEmbed(key, embed[key]);
-                case "author":
-                case "footer":
-                case "image":
-                case "provider":
-                case "thumbnail":
-                case "video":
-                    return handleObjectEmbed(key, embed[key]);
-                case "description":
-                case "title":
-                case "type":
-                case "url":
-                    return handleStringEmbed(key, embed[key]);
-            }
-        }).join("")
+  const embedCommandArray = Object.keys(embed)
+    .map((key: string): any => {
+      let returnVal;
 
-    return addCommandWrapper(
-        "$embed := cembed",
-        `( sdict\n${embedCommandArray})`
-    );
+      switch (key) {
+        case "color":
+          returnVal = handleColorEmbed(key, embed[key]);
+          break;
+        case "fields":
+          returnVal = handleArrayEmbed(key, embed[key]);
+          break;
+        case "author":
+        case "footer":
+        case "image":
+        case "provider":
+        case "thumbnail":
+        case "video":
+          returnVal = handleObjectEmbed(key, embed[key]);
+          break;
+        case "description":
+        case "title":
+        case "type":
+        case "url":
+          returnVal = handleStringEmbed(key, embed[key]);
+          break;
+      }
+      return returnVal;
+    })
+    .join("");
+
+  return addCommandWrapper(
+    "$embed := cembed",
+    `( sdict\n${embedCommandArray})`
+  );
 };
